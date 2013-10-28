87efc7e361f53382dd248e1996d25969a4674bab
vose <newid@live.cn>
2013-10-28 21:13:37 +0800

    README update main.py update read code ×

--- main.py
+++ main.py
@@ -26,8 +26,8 @@ badguyimg1 = pygame.image.load('resources/images/badguy.png')
 badguyimg = badguyimg1
 healthbar = pygame.image.load('resources/images/healthbar.png')
 health = pygame.image.load('resources/images/health.png')
-gameover =  pygame.image.load('resoures/images/gameover.png')
-youwin = pygame.image.load('resoures/images/youwin.png')
+gameover =  pygame.image.load('resources/images/gameover.png')
+youwin = pygame.image.load('resources/images/youwin.png')
 
 # 4 - Keep looping through
 running = 1
@@ -152,39 +152,39 @@ while running:
         playerpos[0]-=5
     elif keys[3]:
         playerpos[0]+=5
-   # 10 - Win/lose check
-   if pygame.time.get_ticks() >= 90000:
-       running=0
-       exitcode=1
-   if healthvalue <= 0:
-       running=0
-       exitcode=0
-   if acc[1] != 0:
-       accuracy=acc[0]*1.0/acc[1]*100
-   else:
-       accuracy=0
-   # 11 - Win/Lose display
-   if exitcode == 0:
-       pygame.font.init()
-       font = pygame.font.Font(None, 24)
-       text = font.render('Accuracy:'+str(acccuracy)+'%',True,(255,0,0))
-       textRect = text.get_rect()
-       textRect.centerx = screen.get_rect().centerx
-       textRect.centery = screen.get_rect().centery+24
-       screen.blit(gameover,(0,0))
-       screen.blit(text,textRect)
-   else:
-       pygame.font.init()
-       font = pygame.font.Font(None,24)
-       text = font.render('Accuracy:'+str(accuracy)+'%',True,(0,255,0))
-       textRect = text.get_rect()
-       textRect.centerx = screen.get_rect().centerx
-       textRect.centery = screen.get_rect().centery+24
-       screen.blit(youwin,(0,0))
-       screen.blit(text,textRect)
-   while 1:
-       for event in pygame.event.get():
-           if event.type == pygame.QUIT:
-               pygame.quit()
-               exit(0)
-       pygame.display.flip()
+    # 10 - Win/lose check
+    if pygame.time.get_ticks() >= 90000:
+        running=0
+        exitcode=1
+    if healthvalue <= 0:
+        running=0
+        exitcode=0
+    if acc[1] != 0:
+        accuracy=acc[0]*1.0/acc[1]*100
+    else:
+        accuracy=0
+# 11 - Win/Lose display
+if exitcode == 0:
+    pygame.font.init()
+    font = pygame.font.Font(None, 24)
+    text = font.render('Accuracy:'+str(accuracy)+'%',True,(255,0,0))
+    textRect = text.get_rect()
+    textRect.centerx = screen.get_rect().centerx
+    textRect.centery = screen.get_rect().centery+24
+    screen.blit(gameover,(0,0))
+    screen.blit(text,textRect)
+else:
+    pygame.font.init()
+    font = pygame.font.Font(None,24)
+    text = font.render('Accuracy:'+str(accuracy)+'%',True,(0,255,0))
+    textRect = text.get_rect()
+    textRect.centerx = screen.get_rect().centerx
+    textRect.centery = screen.get_rect().centery+24
+    screen.blit(youwin,(0,0))
+    screen.blit(text,textRect)
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit(0)
+    pygame.display.flip()
