8c96cee25b5fb577c1ee2739f6007ceeacfc9091
vose <newid@live.cn>
2013-11-09 10:59:08 +0800

    test

--- Corssin.me/weather.py
+++ Corssin.me/weather.py
@@ -2,9 +2,3 @@
 import urllib2
 import json
 
-city_nu = raw_input("Enter city name:")
-url = 'http://www.weather.com.cn/data/cityinfo/'+city_nu+'.html'
-request = urllib2.Request(url)
-response = urllib2.urlopen(request)
-content = response.read()
-print content
