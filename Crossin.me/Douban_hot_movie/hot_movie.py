f46fbc4bf74f0e77076e3496dc3d36faa7bf1ef5
vose_linux <newid@live.cn>
2013-11-15 22:26:49 +0800

    卡住了....看看基础文档去

--- Crossin.me/Douban_hot_movie/hot_movie.py
+++ Crossin.me/Douban_hot_movie/hot_movie.py
@@ -1,11 +1,14 @@
+#coding:utf-8
 import urllib2
 import time #for wait times
 
 
 #Get each page movie in this version just do only one page
+#init args
 page = 0
 url = "http://movie.douban.com/tag/2011?start=%d&type=T"%(page)
 document = ""
+movie_list = []
 
 openurl = urllib2.urlopen(url)
 content = openurl.read()
@@ -13,3 +16,4 @@ tag_1 = content.find('<div class="">')
 tag_2 = content.find('<div class="paginator">')
 content = content[(tag_1+len('<div class="")')):tag_2]
 print content
+
