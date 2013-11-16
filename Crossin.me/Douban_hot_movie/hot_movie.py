cee76954cae611823764735986ea0bcafdabd1b1
vose_linux <newid@live.cn>
2013-11-16 23:16:38 +0800

    regex is really usefull

--- Crossin.me/Douban_hot_movie/hot_movie.py
+++ Crossin.me/Douban_hot_movie/hot_movie.py
@@ -1,14 +1,16 @@
 #coding:utf-8
 import urllib2
 import time #for wait times
+import re
 
 
-#Get each page movie in this version just do only one page
+#Get each page of movie ----In this version just do only one page
 #init args
 page = 0
 url = "http://movie.douban.com/tag/2011?start=%d&type=T"%(page)
 document = ""
-movie_list = []
+#list [movie_name,..]
+#dictionary movie_name:movie_url,movie_rating,movie_review
 
 openurl = urllib2.urlopen(url)
 content = openurl.read()
@@ -16,4 +18,11 @@ tag_1 = content.find('<div class="">')
 tag_2 = content.find('<div class="paginator">')
 content = content[(tag_1+len('<div class="")')):tag_2]
 print content
-
+#looking for movie by regex
+temp_name = re.findall('title=".*"',content)
+for i in temp_name:
+    name=i[len('title="'):-1]
+    name={}
+temp_url = re.findall('<a href=.* ',content)
+temp_rating = re.findall('rating_nums.*<',content)
+temp_review = re.findall('<span class="pl".*<',content)
