b9bba91ad3e348edbff2218ac431a8ce22420697
vose_linux <newid@live.cn>
2013-11-17 23:03:19 +0800

    bug......

--- Crossin.me/Douban_hot_movie/hot_movie.py
+++ Crossin.me/Douban_hot_movie/hot_movie.py
@@ -17,19 +17,25 @@ content = openurl.read()
 tag_1 = content.find('<div class="">')
 tag_2 = content.find('<div class="paginator">')
 content = content[(tag_1+len('<div class="")')):tag_2]
-print content
+print "Get content from HTML"
 #looking for movie by regex 错了，因该根据电影名，然后来划出那部电影的范围，然后获取各值，然后才是赋值，不是获取全部，然后一个个去变列表再赋值
-temp_name = re.findall('title=".*"',content)
-for i in temp_name:
-    name=i[len('title="'):-1]
-    name={}
-temp_url = re.findall('<a href=".*"',content)
-for i in temp_url:
-    name=i[len('<a href="'):-1]
-    global vaule=[name] 
-temp_rating = re.findall('rating_nums">.*<',content)
-    float(name)=i[len('rating_nums">'):-1]
-    vaule.append(name)
-temp_review = re.findall('<span class="pl".*<',content)
-for i in vaule:
-    print i
+movie_dic = {}
+vaule = []
+#
+temp_movie = re.findall('<table[\s\S]*?table>',content)
+print "Split movie from content"
+for i in temp_movie:
+    vaule = []
+    temp_url = (re.search('<a href=".*?"',i)).group(0)[len('<a href="'):-1]
+    vaule.append(temp_url)
+    print temp_url
+    temp_rating = (re.search('rating_nums">.*?<',i)).group(0)[len('rating_nums">'):-1]
+    float(temp_rating)
+    vaule.append(temp_rating)
+    print temp_rating
+    temp_review = (re.search('<span class="pl".*?\)',i)).group(0)[len('<span class="pl">\)'):-4]
+    vaule.append(temp_review)
+    print temp_review
+    break
+    temp_name = (re.search('title=".*?"',i)).group(0)[len('title="'):-1]
+    movie_dic[temp_name]=value
