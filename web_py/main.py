19fab9ce215bfcb05d5e6a5677d785cc52e749ae
vose <newid@live.cn>
2013-12-10 23:37:03 +0800

    -. -

--- web_py/main.py
+++ web_py/main.py
@@ -1,12 +1,18 @@
-﻿import  web
+﻿#coding:utf-8
+import  web
 
-render = web.template.render('templates/')
+render = web.template.render('templates/')#获得模板路径
 urls = ('/', 'index')
+#urls = ('/(.*)', 'index')
+#这个正则我理解为/主页面后一个单词(参数)
 
 class index:
     def GET(self):
-        name = "Bob"
-        return render.index(name)
+        #name = "Bob"
+        i = web.input(name=None)#i.name默认值为None
+        return render.index(i.name)
+    #def GET(self,name):
+    #    return render.index(name)
         
 if __name__ == "__main__":
     app = web.application(urls, globals())
