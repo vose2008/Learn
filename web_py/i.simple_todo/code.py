fa2ce26c706a62498f6d1b49acbb81c8c16cf964
vose_linux <newid@live.cn>
2013-12-14 22:38:13 +0800

    toooo hard

---
+++ web_py/i.simple_todo/code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#coding:utf-8
+import web
+from config.url import urls
+#可以看作config的url包，url包的urls类
+#from i_config import url 这样写下边的urls就是url.urls
+
+app = web.application(urls,globals())
+render = web.template.render('templates/')
+
+class index:
+    def GET(self):
+        return render.index()
+
+if __name__ == "__main__":
+    app.run()
