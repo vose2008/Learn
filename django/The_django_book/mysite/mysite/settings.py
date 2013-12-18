f3c51c03b413bdef9302ba03bc736a5028f43d9d
vose <newid@live.cn>
2013-12-18 23:13:13 +0800

    Chap 04 In render_to_response() error

--- django/The_django_book/mysite/mysite/settings.py
+++ django/The_django_book/mysite/mysite/settings.py
@@ -80,3 +80,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+TEMPLATE_DIRS = (
+    'E:/GitHub/python/django/The_django_book/mysite/mysite/templates',
+)
--- django/The_django_book/mysite/mysite/views.py
+++ django/The_django_book/mysite/mysite/views.py
@@ -3,6 +3,8 @@
 
 import datetime
 from django.http import HttpResponse, Http404
+from django.template.loader import get_template
+from django.template import Context
 
 def hello(request):
     #assert False
@@ -10,7 +12,9 @@ def hello(request):
 
 def current_datetime(request):
     now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>"%now
+    #html = "<html><body>It is now %s.</body></html>"%now
+    t = get_template('current_datetime.html')
+    html = t.render(Context({'current_date':now}))
     return HttpResponse(html)
 
 def hours_ahead(request,offset):
