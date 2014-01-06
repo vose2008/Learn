f11b9d4dd1474d9533f6694dacc93c38eb81a6e0
vose_linux <newid@live.cn>
2014-01-07 00:17:20 +0800

    hard way...

--- django/The_django_book/mysite/books/templatetags/poll_extras.py
+++ django/The_django_book/mysite/books/templatetags/poll_extras.py
@@ -1,4 +1,5 @@
 from django import template
+import datetime
 
 register = template.Library()
 
@@ -11,3 +12,21 @@ register.filter('cut',cut)
 def boom(value):
     value += " test^.^"
     return value
+
+class   CurrentTimeNode(template.Node):
+    def __init__(self, format_string):
+        self.format_string = str(format_string)
+    def render(self, context):
+        now = datetime.datetime.now()
+        return now.strftime(self.format_string)
+
+@register.tag(name="current_time")
+def do_current_time(parser, token):
+    try:
+        tag_name, format_string = token.split_contents()
+    except ValueError:
+        msg = '%r tag requires a single argument'% token.split_contents()[0]
+        raise template.TemplateSyntaxError(msg)
+    return CurrentTimeNode(format_string[1:-1])
+
+#register.tag('current_time', do_current_time)
