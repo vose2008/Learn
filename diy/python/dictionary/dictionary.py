24d128fc106f8bc8b73c0196d04993717b41777c
vose <newid@live.cn>
2014-03-26 11:23:44 +0800

    done 50%

--- diy/python/dictionary/dictionary.py
+++ diy/python/dictionary/dictionary.py
@@ -8,7 +8,7 @@ year_leap = False
 if year%400==0 or (year%4==0 and year%100!=0):
     year_leap = True
 
-def get_days(month,days):
+def get_month_days(month,days):
     i = 0
     while (i<days):
         i+=1
@@ -19,18 +19,22 @@ def get_days(month,days):
         print str(year)+" "+str(month)+" "+str(i)
         i = int(i)
         month = int(month)
- 
-if year_leap:
-    month=0
+
+def get_year_days(year):
+    month = 0
     while (month<12):
-        month+=1
+        month += 1
         if month==1 or month==3 or month==5 or month==7 or month==8 or month==10 or month==12:
             days = 31
-            get_days(month,days)
+            get_month_days(month,days)
         if month==2:
-            days = 29
-            get_days(month,days)
+            if year_leap:
+                days = 29
+            else:
+                days = 28
+            get_month_days(month,days)
         if month==4 or month==6 or month==9 or month==11:
             days = 30 
-            get_days(month,days)
+            get_month_days(month,days)
 
+get_year_days(year)
